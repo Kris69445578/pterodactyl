3bc142449e14c911ae35c04913fbb842c3ed5ad7
Kris69445578 <158816305+Kris69445578@users.noreply.github.com>
2026-09-23 01:53:06 +0300

    Update install-pterodactyl.sh

--- install-pterodactyl.sh
+++ install-pterodactyl.sh
@@ -12,8 +12,8 @@ set -e  # stop on first error
 # ---------------------------------------------------------------------------
 # >>> EDIT THESE BEFORE RUNNING <<<
 # ---------------------------------------------------------------------------
-FQDN="bot.jahim.dpdns.org"      # your domain, already pointed at this VPS
-EMAIL="jahimvj1@gmail.com"       # used for SSL cert + admin account
+FQDN="panel1.jahim.dpdns.org"      # your domain, already pointed at this VPS
+EMAIL="klikedklike@gmail.com"       # used for SSL cert + admin account
 ADMIN_USERNAME="jahim"
 ADMIN_FIRSTNAME="TONY"
 ADMIN_LASTNAME="KINGS"
